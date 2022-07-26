7fdf53dff4cf7fecdbdbe9f043cc0d7b041138b0
Karthik <90515800+Karthik-muktevi@users.noreply.github.com>
2022-07-27 00:18:11 +0530

    Add files via upload

---
+++ division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+a = int(input())
+b = int(input())
+print(a//b)
+print(a/b)
+
+
+# In[ ]:
+
+
+
+
---
+++ loops.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+n = int(input())
+for i in range(0,n):
+    print(i**2)
+
+
+# In[ ]:
+
+
+
+
